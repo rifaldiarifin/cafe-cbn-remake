0704927fa4ce106791a78e0795fe66b84d044421
rifaldiarifin <adirifaldiarifin@gmail.com>
2023-10-11 15:54:59 +0700

    create styling css for page-404

--- src/pages/page404.jsx
+++ src/pages/page404.jsx
@@ -1,8 +1,22 @@
+import Button from '../components/Elements/Button'
 import { useDocumentTitle } from '../hooks/useDocumentHandler'
 
 const Page404 = () => {
   useDocumentTitle('404')
-  return <h1>Page 404</h1>
+  return (
+    <div className="page-404">
+      <div className="box dsp-flex fl-colm justify-center align-itms-center">
+        <div className="char-404">
+          4<span className="searchicon" />4
+        </div>
+        <h1>Page not found</h1>
+        <p>&</p>
+        <Button type="hyperlink" to="/" style="fill" color="classic" height="50px">
+          Back to Home
+        </Button>
+      </div>
+    </div>
+  )
 }
 
 export default Page404
